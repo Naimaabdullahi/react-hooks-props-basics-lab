f89ef346d9bcf8e8bd309124897d672e273198b5
Naimaabdullahi <naima.noor@student.moringaschool.com>
2025-04-19 09:14:23 +0300

    finished

--- src/components/App.js
+++ src/components/App.js
@@ -2,19 +2,35 @@ import React from "react";
 import NavBar from "./NavBar";
 import Home from "./Home";
 import About from "./About";
+import Links from "./Links";  // Ensure you import Links if needed.
 
-// pass this data down as props to the child component(s) that need it!
-import user from "../data/user";
-console.log(user);
+const user = {
+  name: 'Liza',
+  city: 'New York',
+  bio: 'I made this!',
+  color: 'firebrick',
+  links: {
+    github: 'https://github.com/liza',
+    linkedin: 'https://www.linkedin.com/in/liza/'
+  }
+};
 
 function App() {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home 
+        name={user.name}
+        city={user.city}
+        color={user.color}
+      />
+      <About
+        bio={user.bio}
+        links={user.links}  // Pass the entire links object to About
+      />
     </div>
   );
 }
 
 export default App;
+  // Export the entire user object, if needed elsewhere
